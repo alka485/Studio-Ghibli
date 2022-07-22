a302f5f35bc7fab78083db2e5aa9417bfc3f9650
Alka sah <alkasah485@gmail.com>
2022-07-22 14:38:16 -0700

    updation in script files

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,19 @@
+console.log("hello");
 directorNamesEl = document.getElementById("directorname");
 directorListEl = document.getElementById("list of directors");
+
+function getApi(){
+    //fetch request gets a list of all the films by studioGhibli
+    var requestUrl = 'https://ghibliapi.herokuapp.com';
+    console.log(requestUrl);
+    fetch(requestUrl)
+    .then(function (response){
+        return response.json()
+    })
+    .then (function(data){
+        console.log(data);
+    })
+   
+
+}
 
